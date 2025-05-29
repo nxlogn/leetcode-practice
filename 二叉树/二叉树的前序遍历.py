2a22a98d315f18f044c00be7fee4a553a6defbe2
nxlogn <553403830@qq.com>
2025-05-29 23:13:19 +0800

    更新二叉树

--- 二叉树/二叉树的前序遍历.py
+++ 二叉树/二叉树的前序遍历.py
@@ -7,5 +7,22 @@ class TreeNode:
         self.right = right
         
 class Solution:
+
+    def __init__(self) -> None:
+        self.res = []
+
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.traverse(root)
+        return self.res
+
+    def traverse(self, root:TreeNode) -> None:
+        # 空节点
+        if not root:
+            return
+        
+        # 当前节点操作
+        self.res.append(root.val)
+        # 递归左右子节点
+        self.traverse(root.left)
+        self.traverse(root.right)
         
